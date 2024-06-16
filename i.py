05a4a7e7e6b717c829c7053b7c51be7b5725523d
NayDweNaing <ndnaing2004@gmail.com>
2024-06-16 12:24:02 +0630

    js

--- i.py
+++ i.py
@@ -1,2 +1,19 @@
-print ("Only a fool makes a click...")
+const display = document.getElementbyID("display");
+
+function appendToDIsplay(input){
+    display.avlue += input;
+}
+
+funtion clearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error"
+    }
+}
 
